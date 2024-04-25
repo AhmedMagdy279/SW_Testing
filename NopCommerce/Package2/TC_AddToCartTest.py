d13dd58876bbff1a25eca2a00fa2972ba67fd872
Megz <ahmed.magdy27998@gmail.com>
2024-04-25 13:25:17 +0200

    Add to cart Tests uploaded

--- NopCommerce/Package2/TC_AddToCartTest.py
+++ NopCommerce/Package2/TC_AddToCartTest.py
@@ -1,32 +1,86 @@
+import time
 import unittest
-import random
+import sys
+import os
 from selenium import webdriver
+from selenium.webdriver import ActionChains
 from selenium.webdriver.common.by import By
 from Test_Suites.SheetsAutomation import SheetsAutomation
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import Select
+from TestBase.TC_Data import TC0_Data
+from pathlib import Path
 
+parent_folder = os.path.dirname(os.path.abspath(__file__))
+sys.path.append(parent_folder)
+TB_Path = os.path.join(Path(parent_folder).parent.absolute(), "TestBase")
+
+# Note: Add-to-Cart Buttons on item card functionality is slow on interacting,
+# it needs double clicks to add single time to cart. I tried different techniques to work around but still the same.
+# techniques tried: (Implicit/Explicit wait, time.sleep, hover-click, scroll down-click, refresh-click)
 
 class AddToCartTest(unittest.TestCase):
     def setUp(self):
         self.__driver = webdriver.Firefox()
-        self.__driver.get("https://sso.teachable.com/secure/9521/identity/login/password")
-        self.__sheet = SheetsAutomation('../TestSuite/LoginTest_Report.xlsx')
-        self.allItems = self.__driver.find_elements(By.XPATH, "//div[@class='product']")
+        self.__driver.get("https://demo.nopcommerce.com/")
+        self.__action = ActionChains(self.__driver)
+        excel_path = os.path.join(TB_Path, "TC_AddToCartTest_Report.xlsx")
+        self.__sheet = SheetsAutomation(excel_path)
 
     def tearDown(self):
         self.__sheet.save()
-        self.__driver.close()
         self.__driver.quit()
 
-    def test_add_all_items_random_amount(self):         # TC_01
-        self.assertTrue(True)
+    def test_add_single_item_all_sections(self):  # Comprehensive Testing (might be added later)
+        self.assertTrue(True)  # as it will be time-consuming to pick item from 13 pages.
 
-    def test_search_random_item_and_addToCart(self):    # TC_02
-        self.assertTrue(True)
+    def test_searchANDadd_item_no_attributes(self):  # TC_02. Search & pick 'HTC One Mini BLue'
+        self.__driver.find_element(By.ID, "small-searchterms").send_keys("HTC One")
+        self.__driver.find_element(By.XPATH, "//button[text()='Search']").click()
+        wait = WebDriverWait(self.__driver, 3)  # Wait for 3 seconds (adjust as needed)
+        # add_to_cart = wait.until(
+        #     EC.element_to_be_clickable((By.XPATH,
+        #                                 "//a[text()='HTC One Mini Blue']/../../div[@class='add-info']/div["
+        #                                 "@class='buttons']/button[text()='Add to cart']"))
+        # )
+        add_to_cart = self.__driver.find_element(By.XPATH,
+                                                 "//a[text()='HTC One Mini Blue']/../../div[@class='add-info']/div["
+                                                 "@class='buttons']/button[text()='Add to cart']")
+        # self.__action.scroll_by_amount(0, 320).perform()
+        add_to_cart.click()
+        add_to_cart.click()     # weird interaction that item has to be clicked twice after search to act, although it's
+                                # clickable.
+        success_notification = wait.until(
+            EC.presence_of_element_located((By.XPATH, "//div[@class ='bar-notification success']"))
+        )
+        # Assert success based on notification presence
+        self.assertTrue(success_notification.is_displayed(), "Success notification not found")
 
-    def test_click_addToCart_multiple_times(self):      # TC_03. Fail from the second time & on
-        self.assertTrue(True)
+    def test_add_item_with_attributes(self):  # TC_03
+        # Picking 'Build your own computer' with the following:
+        # 2.2 GHZ Intel Pentium Dual-Core E2200
+        # 8 GB RAM, 320 GB HDD
+        # Vista Home, MS Office + Total Commander
+        self.__driver.find_element(By.XPATH, "//a[text()='Build your own computer']").click()
+        drp_dwn = Select(self.__driver.find_element(By.ID, "product_attribute_1"))
+        drp_dwn.select_by_visible_text("2.5 GHz Intel Pentium Dual-Core E2200 [+$15.00]")
+        drp_dwn = Select(self.__driver.find_element(By.ID, "product_attribute_2"))
+        drp_dwn.select_by_visible_text("8GB [+$60.00]")
+        self.__driver.find_element(By.ID, "product_attribute_3_6").click()
+        self.__driver.find_element(By.ID, "product_attribute_4_8").click()
+        if not self.__driver.find_element(By.ID, "product_attribute_4_8").is_selected():
+            self.__driver.find_element(By.ID, "product_attribute_4_8").click()
+        self.__driver.find_element(By.ID, "product_attribute_5_12").click()
+        self.__driver.find_element(By.ID, "add-to-cart-button-1").click()
+        wait = WebDriverWait(self.__driver, 3)  # Wait for 3 seconds (adjust as needed)
+        success_notification = wait.until(
+            EC.presence_of_element_located((By.XPATH, "//div[@class ='bar-notification success']"))
+        )
+        # Assert success based on notification presence
+        self.assertTrue(success_notification.is_displayed(), "Success notification not found")
 
-    def test_remove_random_item_from_cart(self):        # TC_04
+    def test_remove_item_from_cart(self):  # TC_04
         self.assertTrue(True)
 
 
