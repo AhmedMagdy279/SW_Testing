f1e7ada5da3803e7f0150b88d46d4cba5328bd56
Megz <ahmed.magdy27998@gmail.com>
2024-04-29 21:13:59 +0300

    Updated Excel reports with test case data file

--- NopCommerce/TestBase/TC_Data.py
+++ NopCommerce/TestBase/TC_Data.py
@@ -1,4 +1,4 @@
-TC0_Data = {
+TC0_Data = {                # login TC
     'Email': {'row': 2, 'col': 3},
     'Password': {'row': 2, 'col': 4},
     'Actual': {'row': 2, 'col': 6},
@@ -6,7 +6,7 @@ TC0_Data = {
     'Comments': {'row': 2, 'col': 8}
 }
 
-TC1_Data = {
+TC1_Data = {                # sign up TC
     'FName': {'row': 2, 'col': 3},
     'LName': {'row': 2, 'col': 4},
     'Email': {'row': 2, 'col': 5},
@@ -16,4 +16,8 @@ TC1_Data = {
     'Comments': {'row': 2, 'col': 10},
 }
 
-
+TC2_Data = {                # add to Cart TC
+    'Actual': {'row': 2, 'col': 4},
+    'P/F': {'row': 2, 'col': 5},
+    'Comments': {'row': 2, 'col': 6}
+}
